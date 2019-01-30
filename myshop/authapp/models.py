43e60a9a6bbc15fb75b7d94524625e7713acb022
gk90 <georgykorchgain@mail.ru>
2019-01-30 18:48:28 +0300

    structure change

--- myshop/authapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User, AbstractUser
-
-# Create your models here.
-
-class ShopUser(AbstractUser):
-    avatar = models.ImageField(upload_to='users_avatar', blank=True)
-    age = models.PositiveIntegerField(verbose_name= 'возраст')
